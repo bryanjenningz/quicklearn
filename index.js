15cb4adfab14d0f2938b88eebe2a797a1b98def4
Bryan Jennings <bryanjenningz@gmail.com>
2016-01-24 00:52:44 -0800

    Logging in and out updates logged in users list correctly

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = {};
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
+  socket.on('disconnect', function() {
+    delete users[socket.id];
+    io.emit('update users', users);
+  });
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
+  });
+  socket.on('update info', function(info) {
+    users[socket.id] = info;
+    io.emit('update users', users);
   });
 });
 
